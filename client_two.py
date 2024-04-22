1022032026a30854883eb5ec2c2768c29b669090
abanoubnagyabdallah <cv676744@gmail.com>
2024-04-22 14:20:18 +0200

    first commit

---
+++ client_two.py
@@ -0,0 +1,42 @@
+import socket
+import threading
+
+host = socket.gethostname()
+port = 12345
+serv_add=(host,port)
+cli=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+cli.connect(serv_add)
+nickname=input('Enter your name :')
+
+def send():
+    while True:
+        try:
+            msg = nickname + ' : ' + input()
+            cli.sendall(msg.encode())
+        except:
+            print('Send Error')
+
+
+
+def receive():
+    while True:
+        try:
+            msg = cli.recv(1024)
+            if msg == 'Nick':
+                cli.send(nickname.encode())
+            else:
+                print(msg.decode())
+        except:
+            print('receive Error')
+            cli.close()
+            break
+
+
+
+thread_send = threading.Thread(target=send,args=())
+thread_send.start()
+
+thread_receive = threading.Thread(target=receive,args=())
+thread_receive.start()
+
+
---
+++ server.py
@@ -0,0 +1,63 @@
+import socket
+import threading
+
+host =socket.gethostname()
+port =12345
+serv_add=(host,port)
+soc = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+soc.bind(serv_add)
+soc.listen()
+clients=[]
+nicknames=[]
+
+
+
+def broadcast(message):
+    for cli in clients:
+        cli.send(message.encode())
+    return
+
+
+def receive(cli):
+    while True:
+        try:
+            msg = cli.recv(1024)
+            broadcast(msg.decode())
+        except:
+            print('Error occurs')
+            index = clients.index(cli)
+            clients.remove(cli)
+            nickname=nicknames[index]
+            broadcast(nickname+' Left The chat room')
+            nicknames.remove(nickname)
+            break
+
+
+def handle():
+    while True:
+        cli,add=soc.accept()
+        cli.send('Nick'.encode())
+        nickname=cli.recv(1024).decode()
+        nicknames.append(nickname)
+        clients.append(cli)
+        broadcast(nickname+' join to chat room')
+        thread = threading.Thread(target=receive,args=(cli,))
+        thread.start()
+
+print('Server Waiting')
+handle()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
